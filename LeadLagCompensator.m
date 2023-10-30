d5eeaf68a42faf98d32016b4562422edfe008ba4
DavidChen0429 <chenzk001@outlook.com>
2023-10-30 17:46:50 +0100

    lead-lag_compensator

--- LeadLagCompensator.m
+++ LeadLagCompensator.m
@@ -5,42 +5,55 @@ G = 1/(s^2+2*s+5);
 H = feedback(G, -1);
 
 %% Before adding compensator
-figure(1);
+figure('Name', 'Original system');
 subplot(3,1,1)
 step(H)
 subplot(3,1,2)
 rlocus(H)
 subplot(3,1,3)
-bode(H)
+bode(G)
 
 %% Adding a lag compensator
 C1 = (s+1)/(s+2);
 H1 = feedback(C1*G, -1);
-figure(2);
+figure('Name', 'Lag compensator');
 subplot(3,1,1)
 step(H1)
 subplot(3,1,2)
 rlocus(H1)
 subplot(3,1,3)
-bode(H1)
+bode(C1*G)
 % The root locus moves right, results in less stability, slow down settling 
 % time
 
 %% Add a lead compensator
 C2 = (s+2)/(s+1);
 H2 = feedback(C2*G, -1);
-figure(3);
+figure('Name', 'Lead compensator');
 subplot(3,1,1)
 step(H2)
 subplot(3,1,2)
 rlocus(H2)
 subplot(3,1,3)
-bode(H2)
+bode(C2*G)
 % The root locus move left, results in more stability, speed up settling
 % time
 
+%% Add a pole
+%{
+C3 = (1)/(s+1);
+H3 = feedback(C3*G, -1);
+figure('Name', 'Add a pole');
+subplot(3,1,1)
+step(H3)
+subplot(3,1,2)
+rlocus(H3)
+subplot(3,1,3)
+bode(C3*G)
+%}
+
 %% Visualization comparsion
-figure(4)
+figure('Name', 'Compare step response')
 step(H)
 hold on
 step(H1)
