545cfd5e5857daf0e47347fedac0f1dd9b2bf7ef
DavidChen0429 <chenzk001@outlook.com>
2023-11-20 16:41:25 +0100

    add simulink file for discrete control

--- Discretize.m
+++ Discretize.m
@@ -9,8 +9,36 @@ numerator = 4000;
 denominator = [1, 30, 200, 0];
 L = tf(numerator, denominator); 
 
-%% Discritize
+%% Discretizes system
 [A,B,C,D] = tf2ss(numerator, denominator);
 ss_c = ss(A,B,C,D);
 Ts = 1/50;
 ss_d = c2d(ss_c, Ts);
+Ad = ss_d.A;
+Bd = ss_d.B;
+Cd = ss_d.C;
+Dd = ss_d.D;
+
+%% Discretizes controller
+
+% continuous reference tracking
+Kp = 0.49;
+Ki = 0;
+Kd = 0.08;
+Tf = 0.01;
+C_crt = pid(Kp, Ki, Kd, Tf);
+% continuous disturbance rejection 
+Kp = 2.9;
+Ki = 15;
+Kd = 0.3;
+Tf = 0.01;
+C_cdr = pid(Kp, Ki, Kd, Tf);
+
+% discrete reference tracking (bode compare, step compare)
+C_drt_zoh = c2d(C_crt, 0.02, 'zoh');
+C_drt_tustin = c2d(C_crt, 0.02, 'tustin');
+C_drt_matched = c2d(C_crt, 0.02, 'matched');
+% discrete disturbance rejection (bode compare, step compare)
+C_ddr_zoh = c2d(C_cdr, 0.02, 'zoh');
+C_ddr_tustin = c2d(C_cdr, 0.02, 'tustin');
+C_ddr_matched = c2d(C_cdr, 0.02, 'matched');
--- DisturbanceRejection.m
+++ DisturbanceRejection.m
@@ -9,12 +9,12 @@ numerator = 4000;
 denominator = [1, 30, 200, 0];
 G = tf(numerator, denominator); 
 
-Kp = 0.49;
-Ki = 1;
-Kd = 0.08;
-%Kp = 2.9;
-%Ki = 15;
-%Kd = .3;
+%Kp = 0.49;
+%Ki = 1;
+%Kd = 0.08;
+Kp = 2.9;
+Ki = 15;
+Kd = .3;
 Tf = 0.01;
 C = pid(Kp, Ki, Kd, Tf);
 
